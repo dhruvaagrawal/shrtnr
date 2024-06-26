54e4721d98af0508cbcb47cc268595d54a27ec77
Dhruva Agrawal <dhruva1998@gmail.com>
2024-06-26 21:28:01 +0530

    Starting work on the header component

--- client/src/components/shared/header.tsx
+++ client/src/components/shared/header.tsx
@@ -1,18 +1,24 @@
 import { Link } from "@tanstack/react-router"
 import { FC } from "react"
+import ThemeToggle from "@/components/utils/theme-toggle"
 
 interface HeaderProps {}
 
 const Header: FC<HeaderProps> = ({}) => {
     return (
         <div className="flex items-center justify-between gap-2 px-4 py-2">
-            <Link to="/" className="text-2xl font-medium">
-                shrtnr
-            </Link>
-            <div>
-                <Link to="/about" className="[&.active]:font-bold">
-                    About
+            <div className="flex gap-10">
+                <Link to="/" className="text-2xl font-medium">
+                    shrtnr
                 </Link>
+                <div className="flex items-center">
+                    <Link to="/about" className="[&.active]:font-bold">
+                        About
+                    </Link>
+                </div>
+            </div>
+            <div className="flex gap-4">
+                <ThemeToggle />
             </div>
         </div>
     )
--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,10 +1,13 @@
+import { ThemeProvider } from "@/components/utils/theme-provider"
 import { createRootRoute, Outlet } from "@tanstack/react-router"
 import { TanStackRouterDevtools } from "@tanstack/router-devtools"
 
 export const Route = createRootRoute({
     component: () => (
         <div className="font-work-sans">
-            <Outlet />
+            <ThemeProvider defaultTheme="system" storageKey="shrtnr-ui-theme">
+                <Outlet />
+            </ThemeProvider>
             <TanStackRouterDevtools />
         </div>
     ),
